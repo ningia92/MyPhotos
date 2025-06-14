9348807be0f2cc968fe34765f62b8154c68f02cc
ningia <113545072+ningia92@users.noreply.github.com>
2025-06-14 19:10:32 +0200

    Add the Photo class that represents a photo

---
+++ src/main/java/com/myphotos/demo/model/Photo.java
@@ -0,0 +1,30 @@
+package com.myphotos.demo.model;
+
+public class Photo {
+    private int id;
+    private String url;
+
+    public Photo() {
+    }
+
+    public Photo(int id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
